b68c45ad036fc99d38c09e26c799fe4465540302
Sharmarke <somaliwarsan@gmail.com>
2019-12-16 23:30:36 +0300

    added various validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: { message: 'Please provide your full name' }
+  validates :password, confirmation: true, length: { minimum: 8, too_short: 'Password must be at least 8 characters long' }
+  validates :email, uniqueness: true
+  validates :user_name, uniqueness: { case_senstive: false }
 end
